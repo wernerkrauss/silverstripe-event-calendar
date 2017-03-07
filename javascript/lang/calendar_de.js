1efa9700cd95002e52846ae6f236d211b121c8cf
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2017-03-07 09:31:48 +0100

    fix umlaut problem in german js lang file

--- javascript/lang/calendar_de.js
+++ javascript/lang/calendar_de.js
@@ -2,7 +2,6 @@
     $.CalendarWidget.setOptions({
         startOnMonday: false,
         calDaysLabels: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-        calMonthsLabels: ['Januar', 'Februar', 'März', 'April','Mai', 'Juni', 'Juli', 'August', 'September','Oktober', 'November', 'Dezember']
-
+        calMonthsLabels: ['Januar', 'Februar', 'MÃ¤rz', 'April','Mai', 'Juni', 'Juli', 'August', 'September','Oktober', 'November', 'Dezember']
     })
 })(jQuery);
